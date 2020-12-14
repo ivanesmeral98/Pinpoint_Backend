32bbf06977728e02d730b2c6c8f67824c258c11d
Ivan Esmeral <ivanesmeral@Ivans-MacBook-Pro-2.local>
2020-12-14 18:47:37 -0500

    basic ability to add users and connected django rest

--- Pinpoint_backend/settings.py
+++ Pinpoint_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
